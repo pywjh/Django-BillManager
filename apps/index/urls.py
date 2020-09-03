75b487696978b71651ddfd12b2396fd9e536f63f
wjh <1588540514@163.com>
2020-09-04 00:07:27 +0800

    引入了网站的模板文件，静态文件指定

---
+++ apps/index/urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# ========================================
+# Author: wjh
+# Date：2020/9/3
+# FILE: urls
+# ========================================
+
+from django.urls import path, include
+from . import views
+
+app_name = 'index'
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index')
+]
--- apps/index/views.py
+++ apps/index/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+from index.models import BillModel, DayDetailModel
 
 # Create your views here.
+
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'common/base.html')
