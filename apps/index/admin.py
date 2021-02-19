20760f25a5d71b015a877a17875e5acd11559bb9
wjh <1588540514@163.com>
2021-02-19 13:39:29 +0800

    性能优化

--- apps/index/admin.py
+++ apps/index/admin.py
@@ -12,14 +12,14 @@ admin.site.site_header = '我的账单'
 @admin.register(BillModel)
 class BillModelAdmin(admin.ModelAdmin):
     # 设置模型字段，用于Admin后台数据的表头设置
-    list_display = ['date', 'salary', 'budget', 'rent', 'salary_day', 'save_amount', 'note']
+    list_display = ['date', 'salary', 'budget', 'rent', 'salary_day', 'note']
     # 设置可搜索的字段并在Admin后台数据生成搜索框，如有外键应使用双下划线连接两个模型的字段
     search_fields = ['date', 'detail_id__note', 'detail_id__name']
     # 过滤
     list_filter = ['date']
     fieldsets = (  # form页面 显示字段
         (None, {'fields': (
-            'date', 'salary', 'budget', 'rent', 'salary_day', 'save_amount', 'note'
+            'date', 'salary', 'budget', 'rent', 'salary_day', 'note'
         )}),
     )
     # # 设置排序方式
@@ -28,6 +28,7 @@ class BillModelAdmin(admin.ModelAdmin):
 
 @admin.register(DayDetailModel)
 class DayDetailAdmin(admin.ModelAdmin):
+    show_full_result_count = False  # 搜索的时候，在搜出来的结果"xx条结果"后面不显示"总共xx"，耗性能
     list_display = ['date', 'name', 'amount', 'type', 'note']
     search_fields = ['date', 'name', 'type', 'note']
     # 过滤
--- apps/investment/admin.py
+++ apps/investment/admin.py
@@ -6,6 +6,9 @@ from .models import InvestmentModel
 
 @admin.register(InvestmentModel)
 class InInvestmentModelAdmin(admin.ModelAdmin):
+    # 搜索的时候，在搜出来的结果"xx条结果"后面不显示"总共xx"，耗性能
+    # 但是好像基本也用不上搜索功能..
+    show_full_result_count = False
     list_display = ['date', 'earnings']
     # 过滤
     list_filter = ['date']
